792ed8420e4535d046d9d9c364a8e4a574709efa
Firas Naber <firasnaber@gmail.com>
2025-05-31 22:11:30 -0500

    feat: add default sorting by tour creation date

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -14,8 +14,10 @@ exports.getAllTours = async (req, res) => {
     let query = Tour.find(queryStr);
 
     if (req.query.sort) {
-      console.log(req.query.sort);
-      query = query.sort(req.query.sort);
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
     }
 
     const allTours = await query;
